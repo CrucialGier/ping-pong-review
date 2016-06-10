dd18d77b1c399cc9d70fbd9feac5fd4203e15b2a
Stewart Cole <stewart@>
2016-06-10 10:22:12 -0700

    all js works and prints each number or output as a li in the result ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
+// Front End? //
+
 $(document).ready(function() {
-  $("#getResult").click(function() {
-    
+  $("#getResult").click(function(event) {
+    event.preventDefault();
+    $("#result").empty();
+    getResult(input);
   });
 });
+
+// Back End? //
+
+var getResult = function(input) {
+  var input = parseInt($("#input").val());
+  for (var i = 0; i < input; i++) {
+    if ((i + 1) % 3 === 0 && (i + 1) % 5 === 0) {
+      $("#result").append("<li>" + "Ping-Pong!!" + "</li>");
+    } else if ((i + 1) % 3 === 0) {
+      $("#result").append("<li>" + "Ping!" + "</li>");
+    } else if ((i + 1) % 5 === 0) {
+      $("#result").append("<li>" + "pong!" + "</li>");
+    } else {
+      $("#result").append("<li>" + (i + 1) + "</li>");
+    };
+  };
+};
