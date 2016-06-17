c2bf4ff588b30ac4f403bfb2d393228b53434d84
Stewart Cole <stewart@>
2016-06-17 09:10:03 -0700

    updated js to be easier to manage and front and backend logic are more seperated

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,29 @@
-// Front End? //
+var input;
+var pingPong = [];
+
+var getResult = function(input) {
+  pingPong = [];
+  for (var i = 1; i <= input; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      pingPong.push("ping-pong");
+    } else if (i % 3 === 0) {
+      pingPong.push("ping");
+    } else if (i % 5 === 0) {
+      pingPong.push("pong");
+    } else {
+      pingPong.push(i);
+    };
+  };
+};
 
 $(document).ready(function() {
   $("#getResult").click(function(event) {
     event.preventDefault();
+    input = parseInt($("#input").val());
     $("#result").empty();
     getResult(input);
+    pingPong.forEach(function(current) {
+      $("#result").append("<li>" + current + "</li>");
+    });
   });
 });
-
-// Back End? //
-
-var getResult = function(input) {
-  var input = parseInt($("#input").val());
-  for (var i = 0; i < input; i++) {
-    if ((i + 1) % 3 === 0 && (i + 1) % 5 === 0) {
-      $("#result").append("<li>" + "Ping-Pong!!" + "</li>");
-    } else if ((i + 1) % 3 === 0) {
-      $("#result").append("<li>" + "Ping!" + "</li>");
-    } else if ((i + 1) % 5 === 0) {
-      $("#result").append("<li>" + "pong!" + "</li>");
-    } else {
-      $("#result").append("<li>" + (i + 1) + "</li>");
-    };
-  };
-};
